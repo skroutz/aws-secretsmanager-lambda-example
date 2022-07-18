0fdc1256f1bb5c5d55c1ac4c2023a896e6e35f52
gian2dchris <50832176+gian2dchris@users.noreply.github.com>
2022-07-18 16:29:17 +0300

    secrets-layer: multiple secrets

--- secrets-layer/secrets-layer.sh
+++ secrets-layer/secrets-layer.sh
@@ -15,29 +15,31 @@
 exec ${args[@]}
 
 echo "This is the layer deployed using Github Actions !"
-
 # The name of this script
 name=$(basename $0)
+echo "Name: ${name}"
 
-# # The full path to this script
+# The full path to this script
 fullPath=$(dirname $(readlink -f $0))
+echo "Fullpath: ${fullpath}"
 
-# # The path to the interpreter and all of the originally intended arguments
+# The path to the interpreter and all of the originally intended arguments
 args=("$@")
 
 # # The name of the secret
-secretArn="${SECRET_ARN}"
+# secretArn="${SECRET_ARN}"
 
-# # The region to use for the API calls
+# The region to use for the API calls
 region="${SECRET_REGION}"
+echo "Region ${region}"
 
-# # The role to use for API calls
+# The role to use for API calls
 # roleName="${ASSUME_ROLE_ARN}"
 
-# # The timeout for API calls
+# The timeout for API calls
 # timeout="${API_TIMEOUT}"
 
-# # Create a temp file to hold values to be exported
+# Create a temp file to hold values to be exported
 tempFile=$(mktemp /tmp/${name}.XXXXXXX)
 last_cmd=$?
 
@@ -46,23 +48,25 @@ if [[ ${last_cmd} -ne 0 ]]; then
 	exit 1
 fi
 
-# # Get the secret value by calling the Go executable
-echo "Fullpath: ${fullpath}"
-values=$(${fullPath}/get-secret -r "${region}" -s "${secretArn}")
-last_cmd=$?
-
-# # Verify that the last command was successful
-if [[ ${last_cmd} -ne 0 ]]; then
-    echo "Failed to setup environment for Secret ${secretArn}"
-    exit 1
-fi
-
-# # Read the data line by line and export the data as key value pairs 
-# # and environmental variables
-echo "${values}" | while read -r line; do 
+# Get the secret value by calling the Go executable
+SECRETS=$(yq e -o=json ./secrets.yaml | jq '.secrets' | jq -c '.[]')
+echo "${SECRETS}"
+for s in $SECRETS;do
+    secretArn=$(echo $s | jq -r -c .valueFrom)
+    echo "Secret ARN: ${secretArn}"
+    keyval=$(${fullPath}/get-secret -r "${region}" -s ${secretArn})
+    # Verify that the last command was successful
+    last_cmd=$?
+    if [[ ${last_cmd} -ne 0 ]]; then
+        echo "Failed to setup environment for Secret ${secretArn}"
+        exit 1
+    fi
+    # # Read the data line by line and export the data as key value pairs 
+    # # and environmental variables
+    # echo "${values}" | while read -r line; do 
     
-    # Split the line into a key and value
-    ARRY=(${line//|/ })
+    #     # Split the line into a key and value
+    ARRY=(${keyval//|/ })
 
     # Capture the kay value
     key="${ARRY[0]}"
@@ -74,9 +78,10 @@ echo "${values}" | while read -r line; do
     # The split man have broken the data into multiple values.  This will force the
     # data to be rejoined.
     value="${ARRY[@]}"
-    
+        
     # Save as an env var to the temp file for later processing
     echo "export ${key}=\"${value}\"" >> ${tempFile}
+    # done
 done
 
 # Source the temp file to read in the env vars
@@ -95,8 +100,8 @@ else
     args=("${AWS_LAMBDA_EXEC_WRAPPER}" "${args[@]}")
 fi
 
-# # Remove the temp file
+# Remove the temp file
 rm ${tempFile} > /dev/null 2>&1
 
-# # Execute the next step
+# Execute the next step
 exec ${args[@]}
