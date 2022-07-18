254a670d72dc83d49cddc9af5660607d2ea683b2
gian2dchris <50832176+gian2dchris@users.noreply.github.com>
2022-07-18 18:10:46 +0300

    rb-function: multiple secrets

--- rb-function/lambda_function.rb
+++ rb-function/lambda_function.rb
@@ -1,9 +1,10 @@
 require 'json'
 
-$secret = ENV["SECRET_VALUE"]
+$secret1 = ENV["SECRET_VALUE_1"]
+$secret2 = ENV["SECRET_VALUE_2"]
     
 def lambda_handler(event:, context:)
     
     # TODO implement
-    { statusCode: 200, body: JSON.generate('Hello from Ruby Lambda Function deployed with TF ! This is a secret: %s' %[$secret] ) }
+    { statusCode: 200, body: JSON.generate('Hello from Ruby Lambda Function deployed with TF ! This is a secret: %s. This is a secret: %s.' % [$secret1, $secret2]) }
 end
