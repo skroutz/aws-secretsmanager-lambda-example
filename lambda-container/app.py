8c51d715a3ce0b064239ad11a9a59c6950596276
gian2dchris <50832176+gian2dchris@users.noreply.github.com>
2022-08-04 14:53:04 +0300

    example: lambda-container dockerfile /w secrets extension

---
+++ lambda-container/app.py
@@ -0,0 +1,13 @@
+import json
+import subprocess
+import os
+
+def lambda_handler(event, context):    
+    
+    secret1 = os.environ.get('SECRET_VALUE_1', 'Not Set')
+    secret2 = os.environ.get('SECRET_VALUE_2', 'Not Set')
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f'Hello from Python Lambda Function deployed with TF ! This is a secret1: {secret1}! This is a secret2: {secret2}!')
+    }
