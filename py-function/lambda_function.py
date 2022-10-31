871fa097d4b61aeef1a92c266e2a749d4f232fc4
Giannis Christinakis <50832176+gian2dchris@users.noreply.github.com>
2022-10-31 17:20:04 +0200

    py function

---
+++ py-function/lambda_function.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+def lambda_handler(event, context):
+    # secret = get_secret()
+    secret1 = os.environ.get('SECRET_VALUE_1', 'Not Set')
+    secret2 = os.environ.get('SECRET_VALUE_2', 'Not Set')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f'Hello from Python Lambda Function deployed with TF ! This is a secret1: {secret1}! This is a secret2: {secret2}!')
+    }
